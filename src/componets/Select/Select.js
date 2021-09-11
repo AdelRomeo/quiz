2904f673881242e07050b1dda3901da22b15e5d6
AdelRomeo <11cage11@mail.ru>
2021-09-12 02:17:11 +0700

    select

---
+++ src/componets/Select/Select.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react'
+import classes from "./Select.module.scss";
+import CreateQuizContext from "../../context/createQuiz/createQuizContext";
+
+function Select({config}) {
+
+  const {setRightAnswerId} = useContext(CreateQuizContext)
+
+  const {label, options} = config
+
+  return (
+    <div className={classes.selectContainer}>
+      <label htmlFor="rightAnswerId">{label}</label>
+      <select id='rightAnswerId' onChange={(event)=>setRightAnswerId(event.target.value)}>
+        {Object.keys(options).map((option) => {
+          return(
+            <option value={options[option]} key={options[option]}>{options[option]}</option>
+          )
+        })}
+      </select>
+    </div>
+  )
+}
+
+export default Select
--- src/context/createQuiz/createQuizState.js
+++ src/context/createQuiz/createQuizState.js
@@ -8,8 +8,6 @@ export default function CreateQuizState({children}) {
   const initialState = {
     //готовый тест
     quizes: [],
-    //готовый вопрос
-    questions: {},
     //шаблон формы
     templateForm: {
       question: {placeholder: 'Ваш вопрос', type: 'text', value: ''},
@@ -18,40 +16,67 @@ export default function CreateQuizState({children}) {
       option3: {placeholder: 'Вариант ответа 3', type: 'text', value: ''},
       option4: {placeholder: 'Вариант ответа 4', type: 'text', value: ''},
     },
+    //правильный вариант ответа
+    rightAnswerId: 1,
   }
 
   const [state, dispatch] = useReducer(CreateQuizReducer, initialState)
 
   //изменение в строке инпута
-  const fillingField = (value, controlName)=>{
+  const fillingField = (value, controlName) => {
     //копия шаблона из state
-    const templateForm = {...state.templateForm}
+    const templateFormCopy = {...templateForm}
     //копия элемента шаблона
-    const item = {...templateForm[controlName]}
+    const item = {...templateFormCopy[controlName]}
     //изменение копии элемента шаблона
     item.value = value
+
+    function Test(name, value){
+      this.name = name
+      this.value = value
+    }
+
+    let obj = new Test(controlName, value)
+
+    let finObj = {}
+
+    finObj = {...finObj, ...obj}
+    console.log(finObj)
+
     //изменение копии шаблона из state
-    templateForm[controlName] = {...item}
+    templateFormCopy[controlName] = {...item}
 
     dispatch({
       type: 'FILLING_FIELD',
-      templateForm
+      templateFormCopy
+    })
+  }
+
+  const setRightAnswerId = (rightId) => {
+    dispatch({
+      type: 'SELECTED_RIGHT_ANSWER_ID',
+      rightId
     })
   }
 
   //клик по кнопке "Добавить вопрос"
-  const addQuestion = (index) => {
-
+  const addQuestion = (question, rightId) => {
+    const quiz = {...question, rightAnswerId: rightId}
     dispatch({
       type: 'ADD_QUESTION',
-      index
+      quiz
     })
+
+    const obj = Object.keys(question).map((item)=>{
+      return {item: question[item].value}
+    })
+
   }
 
-  const {value, templateForm} = state
+  const {templateForm, rightAnswerId} = state
 
   return (
-    <CreateQuizContext.Provider value={{value, templateForm, fillingField}}>
+    <CreateQuizContext.Provider value={{templateForm, rightAnswerId, fillingField, setRightAnswerId, addQuestion}}>
       {children}
     </CreateQuizContext.Provider>
   )
